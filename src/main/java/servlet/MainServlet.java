cbf076912a172ea1f43fa97564186ade0ef73074
StarodubOleksiy <StarodubAleks@gmail.com>
2016-07-23 02:35:54 +0300

    second commit

--- src/main/java/servlet/MainServlet.java
+++ src/main/java/servlet/MainServlet.java
@@ -22,24 +22,22 @@ private String page = new String();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-    System.out.println("UpdateServlet!!!!!!!!!!!");
-       String tasks[] = req.getParameterValues("task");
+        updateResponse(req,  resp);
 
+}
+    private void updateResponse(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+       String tasks[] = request.getParameterValues("task");
         if(tasks != null) {
             for (int i = 0; i < result.size(); ++i) {
                 for (int j = 0; j < tasks.length; ++j)
                     if(result.get(i).getName().equals(tasks[j]))
                         result.remove(i) ;
-                    }
+            }
         }
-
-
-
-        req.setAttribute("styles", result);
-        RequestDispatcher view = req.getRequestDispatcher("headers.jsp");
-        view.forward(req, resp);
-
-
+        request.setAttribute("styles", result);
+        RequestDispatcher view = request.getRequestDispatcher("headers.jsp");
+        view.forward(request, response);
     }
 
     @Override
